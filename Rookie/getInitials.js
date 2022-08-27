02ec32fd82cf817d5491d358bbaacda7e6b54cbf
Ptkstefano <68793444+Ptkstefano@users.noreply.github.com>
2022-08-27 13:26:26 -0300

    Add files via upload

---
+++ Rookie/getInitials.js
@@ -0,0 +1,18 @@
+
+//This functions gets the initials from a person name
+//Example - Elvis Presley returns EP
+
+function getInitials(name){
+
+    let initials = "";
+    initials = name.charAt(0)
+
+    for (let i = 0; i < name.length; i++){
+        if (name.charAt(i) == " "){
+            initials += name.charAt(i+1);
+        }
+    }
+    return initials;
+}
+
+console.log(getInitials("Elvis Presley"));
